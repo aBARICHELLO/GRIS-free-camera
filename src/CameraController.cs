6136ef4b88d420a346d9119244c092ef4ed1911d
BARICHELLO <artur@barichello.me>
2019-08-24 17:15:15 -0300

    Add follow mode

--- src/CameraController.cs
+++ src/CameraController.cs
@@ -8,12 +8,17 @@ namespace Gris {
     [RequireComponent(typeof(Camera))]
     public partial class CameraController : MonoBehaviourSaveGameListener {
 
+        // (...)
+
         void LateUpdate() {
             if (!Utils.IsGameRunning()) {
                 return;
             }
             if (Input.GetKeyDown(KeyCode.Numlock)) {
                 this.noclip = !this.noclip;
+            }
+            if (Input.GetKeyDown(KeyCode.Backspace)) {
+                this.followMode = !this.followMode;
             }
             if (this.noclip) {
                 this.NoclipUpdate(ref this.m_Camera);
@@ -23,18 +28,19 @@ namespace Gris {
         }
 
         void NoclipUpdate(ref Camera camera) {
+            // Speed
             float speedModifier = 1f;
-            if (Input.GetKey(KeyCode.LeftShift)) {
-                speedModifier = 10f;
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.Mouse3)) {
+                speedModifier = 20f;
             }
 
             // Transform
-            if (Input.GetKey(KeyCode.KeypadMinus)) {
+            if (Input.GetKey(KeyCode.KeypadMinus) || Input.mouseScrollDelta.y < 0f) {
                 Vector3 direction = camera.transform.forward;
                 direction.Normalize();
                 camera.transform.position -= Time.deltaTime * noclipCameraSpeed * speedModifier * direction;
             }
-            if (Input.GetKey(KeyCode.KeypadPlus)) {
+            if (Input.GetKey(KeyCode.KeypadPlus) || Input.mouseScrollDelta.y > 0f) {
                 Vector3 direction = camera.transform.forward;
                 direction.Normalize();
                 camera.transform.position += Time.deltaTime * noclipCameraSpeed * speedModifier * direction;
@@ -49,18 +55,27 @@ namespace Gris {
                 direction.Normalize();
                 camera.transform.position += Time.deltaTime * noclipCameraSpeed * speedModifier * direction;
             }
-            if (Input.GetKey(KeyCode.DownArrow)) {
-                Vector3 direction = camera.transform.up;
-                direction.Normalize();
-                camera.transform.position -= Time.deltaTime * noclipCameraSpeed * speedModifier * direction;
-            }
             if (Input.GetKey(KeyCode.UpArrow)) {
                 Vector3 direction = camera.transform.up;
                 direction.Normalize();
                 camera.transform.position += Time.deltaTime * noclipCameraSpeed * speedModifier * direction;
             }
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                Vector3 direction = camera.transform.up;
+                direction.Normalize();
+                camera.transform.position -= Time.deltaTime * noclipCameraSpeed * speedModifier * direction;
+            }
+
+            if (this.followMode) {
+                CameraController.CameraSettings currentSettings = this.GetCurrentSettings();
+                Vector3 vector = this.CalculateCameraPosition(currentSettings, ref this.m_CurrentCameraTransientData, true);
+                camera.transform.position = new Vector3(vector.x, camera.transform.position.y, camera.transform.position.z);
+            }
         }
 
+        // (...)
+
+        bool followMode = false;
         bool noclip = false;
         const float noclipCameraSpeed = 10f;
     }
